dba9042b71862f1bde8067c1db41b2337d4c21df
LightningFrisk <bmxcaleb@gmail.com>
2023-04-10 19:33:17 -0500

    Push finished studio

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -28,6 +28,9 @@ public class BalancedBrackets {
                 brackets++;
             } else if (ch == ']') {
                 brackets--;
+                if(brackets < 0){
+                    return brackets == 0;
+                }
             }
         }
         return brackets == 0;
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,56 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onceSetBalanceBracketsReturnsCorrectTrue() { assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));}
+
+    @Test
+    public void emptyStringReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void wrongOrderBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void onlyOneBracket (){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void checkNested (){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]][]"));
+    }
+
+    @Test
+    public void checkSpecialCharacters (){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("\n"));
+    }
+
+    @Test
+    public void allLeadingBracket (){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[[[[[[[[["));
+    }
+    @Test
+    public void allTrailingBracket (){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]]]]]]]]]"));
+    }
+
+    @Test
+    public void positiveTest (){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Hello World[]"));
+    }
+
+    @Test
+    public void negativeTest (){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Hello World]["));
+    }
+
+    @Test
+    public void onlyOneTrailingBracket (){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
 
 }
